75ff5b3915e57b538037a138db56e4a8eb4d1f46
martengooz <you@example.com>
2018-02-09 15:11:14 +0100

    Clean up config defaults

--- backup_conf.py
+++ backup_conf.py
@@ -3,7 +3,7 @@
 
 # Path to where to store the backed up files and directories.
 # WARNING: All conflicting files or folders in the path will be overwritten
-BACKUP_PATH = "/tmp/backup/"
+BACKUP_PATH = "backup/"
 
 # Specify users homes to backup.
 # Valid values are:
@@ -12,36 +12,35 @@ BACKUP_PATH = "/tmp/backup/"
 #  * USERS = "none"      # No users home will be backed up
 #  * USERS = [   "list",
 #                "of",
-#                "users"]    # You should probably run as root if a list is used
-USERS = ["none"]
+#                "users"]    # You should probably run as root multiple users is used
+USERS = "current"
 
 # A list of custom files/directories to backup
 CUSTOM_FILES = [
-    # Commented out lines will be ingored
+    # Commented out lines will be ignored
     #"/path/to/file/or/directory",
-    #"~/.zshrc",
     #"/media/documents/",
-    "~/.bashrc"
+    #"~/.bashrc"
 ]
 
 # Specify what package config files you want to backup.
-# A list of available packages can be found in default_paths.json
-PACKAGE_CONFIG = [
+# A list of available packages can be found in default_paths.py
+PACKAGES_CONFIG = [
     "ssh",
-    "samba",
-    #"php7.0-cli",
+    #"samba",
     #"php7.0-cgi",
-    #"apache2",
-    #"fail2ban",
     #"ddclient",
-    #"nginx",
-    #"lighttpd",
-    #"ddclient",
-    "cron"
+    #"cron"
 ]
 
-# Override the default path of a packages config files listed in default_paths.json
-OVERRIDE_DEFAULT_PATH = {
-    #"[software]":"/path/to/config",
-    "ssh":"/tmp/ssh/sshd.conf"
+# Add your own custom packages config files.
+# If the package name already exists in default_paths.py it will be overridden
+# with the path specified below.
+CUSTOM_PACKAGES_CONFIG = {
+    # Examples:
+    # "ssh":"/path/to/config",
+    # "otherpackage": [\
+    #     "/otherpackage/first.conf",
+    #     "/otherpackage/second.conf",
+    #     "/otherpackage/confdirectory"],
 }
