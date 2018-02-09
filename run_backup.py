b868efcfa8ec8ab9cab88b9875932786e2758d84
martengooz <you@example.com>
2018-02-09 12:28:49 +0100

    Restructure config

--- run_backup.py
+++ run_backup.py
@@ -1,26 +1,20 @@
 import json
-from pprint import pprint
 import os
+import sys
+import backup_conf
+import default_paths
+import getpass
+import argparse
 from shutil import copytree, rmtree, copy
 
-# Path to store the backup
-backup_path = "/tmp/backup/"
 
-# Names of the software to copy. Available software is in default_paths.json
-software_to_backup = [
-    "ssh",
-    "samba",
-    "php",
-    #"apache2",
-    #"fail2ban",
-    #"ddclient",
-    "nginx",
-    #"lighttpd",
-    #"ddclient",
-    "cron"
-]
-
-default_paths = json.load(open('default_paths.json'))
+# Variables
+defaultPaths = default_paths.PATHS
+backupPath = backup_conf.BACKUP_PATH
+users = backup_conf.USERS
+customFiles = backup_conf.CUSTOM_FILES
+packageConfig = backup_conf.PACKAGE_CONFIG
+overrideDefaultPath = backup_conf.OVERRIDE_DEFAULT_PATH
 
 def getFileList (softwarelist, softwarepaths):
     backupfiles = []
@@ -33,7 +27,7 @@ def getFileList (softwarelist, softwarepaths):
                 backupfiles.append(softwarepaths[software]) # Single file/directory
         except KeyError, e: # If not in softwarepaths
             print "ERROR: Couldn't fint the path(s) for " + str(e) + \
-                " is not in the default_paths.json dictionary. Please add it manually."
+                " is not in the default_paths.py dictionary. Please add it manually."
         except:
             print "ERROR: An error occured when locating the default path for " + software
     return backupfiles
@@ -46,44 +40,54 @@ def copyFiles(files, bpath):
         try:
             if os.path.isdir(src):              # Is a directory
                 if os.path.isdir(dst):          # Directory already exist in dst
-                    print "ERROR: " + dst + " already exists. Will overwrite." #TODO add prompt
+                    print "INFO: " + dst + " already exists. Will overwrite." #TODO add prompt
                     rmtree(dst)
                 elif os.path.isfile(dst):       # Regular file already exist in dst
                     print "ERROR: " + dst + " is a regualar file. Will overwrite." #TODO add prompt
                     os.remove(dst)
                 # Directory does not exist :)
-                print "Copying" + src + " to " + dst
+                print "Copying " + src + " to " + dst
                 copytree(src, dst)
             elif os.path.isfile(src):           # Is a file
                 if os.path.isdir(dst):          # Directory already exists in dst
                     print "ERROR: " + dst + " is a directory. Will overwrite." #TODO add prompt
                     rmtree(dst)
                 elif os.path.isfile(dst):       # Regular file already exists in dst
-                    print "ERROR: File exists. " + dst
+                    print "INFO: " + dst + " already exists. Will overwrite."
                     os.remove(dst)
                 if not(os.path.isdir(bpath + os.path.dirname(src))):
                     os.makedirs(bpath + os.path.dirname(src))
-                print "Copying" + src + " to " + dst
+                print "Copying " + src + " to " + dst
                 copy(src, dst)
             else:
                 print "ERROR: Couldn't find " + src
         except IOError, e:
-            print "ERROR: Permission denied: " + str(e)
+            print str(e)
+        except OSError, e:
+            print e
         except Exception as e:
             raise
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--noroot', action='store_const', const=True)
+parser.parse_args(['--noroot'])
+
 # Check for sudo
-if not(os.getuid() == 0):
-    print "Not sudo, you might not have permission for some files."
+if not(os.getuid() == 0) and not(parser.parse_args().noroot):
+    print "WARNING: Not root, you might not have permission to copy some files."
+    print "If you still want to run without root priveleges, please add the --no-root flag"
+    sys.exit(0)
 
 try:
     print "Creating backup directory"
-    if backup_path.endswith('/'):
-        backup_path = backup_path[:-1]
-    os.makedirs(backup_path)
+    if backupPath.endswith('/'):
+        backupPath = backupPath[:-1]
+    os.makedirs(backupPath)
 except:
     #TODO add prompt if user want to override
     print "Backup directory exists"
 
-filelist = getFileList(software_to_backup, default_paths)
-copyFiles(filelist, backup_path)
+#copyFiles(users)
+filelist = getFileList(packageConfig, defaultPaths)
+copyFiles(filelist, backupPath)
