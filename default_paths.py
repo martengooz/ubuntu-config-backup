5f71b13d78018fb1c3d29a6e85ca2af0be347438
martengooz <you@example.com>
2018-02-09 12:25:03 +0100

    Rename file

---
+++ default_paths.py
@@ -0,0 +1,39 @@
+PATHS = {
+    "apache2": [
+        "/etc/apache2/apache2.conf",
+        "/etc/apache2/conf-available",
+        "/etc/apache2/conf-enabled",
+        "/etc/apache2/mods-available",
+        "/etc/apache2/mods-enabled",
+        "/etc/apache2/sites-available",
+        "/etc/apache2/sites-enabled"
+    ],
+    "cron": [
+        "/etc/cron.d",
+        "/etc/cron.daily",
+        "/etc/cron.monthly",
+        "/etc/cron.weekly",
+        "/etc/crontab"
+    ],
+    "ddclient": "/etc/ddclient/ddclient.conf",
+    "fail2ban": [
+        "/etc/fail2ban/action.d",
+        "/etc/fail2ban/filter.d",
+        "/etc/fail2ban/jail.conf",
+        "/etc/fail2ban/jail.d",
+        "/etc/fail2ban/jail.local"
+    ],
+    "fstab": "/etc/fstab",
+    "lighttpd": "/etc/lighttpd/lighttpd.conf",
+    "nginx": [
+        "/etc/nginx/nginx.conf",
+        "/etc/nginx/sites-available",
+        "/etc/nginx/sites-enabled"
+    ],
+    "php7.0-cgi": "/etc/php/7.0/cgi/php.ini",
+    "php7.0-cli": "/etc/php/7.0/cli/php.ini",
+    "php7.0-fpm": "/etc/php/7.0/fpm/php.ini",
+    "samba": "/etc/samba/smb.conf",
+    "ssh": "/etc/ssh/ssh_config",
+    "sshd": "/etc/ssh/sshd.conf"
+}
